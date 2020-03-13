705df4a4caaabf7baf19aec50f891017c2ce3e02
jdudziec <dudziecjacek@gmail.com>
2020-03-13 15:49:27 +0100

    style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters: boolean;
+  password: string;
+  includeNumbers: boolean;
+  includeSymbols: boolean;
+  length: number;
+
+  constructor() {
+    this.password = '';
+    this.includeLetters = false;
+    this.includeNumbers = false;
+    this.includeSymbols = false;
+    this.length = 0;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value, 10);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+      console.log(this.length);
+    }
+  }
+
+  onButtonClick() {
+    const numbers = '123456789';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '~!@#$%^&*()_+-=';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    const arrOfChars = validChars.split('');
+    const generatedPassword = [];
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.length; i++) {
+      generatedPassword.push(arrOfChars[Math.floor(Math.random() * arrOfChars.length)]);
+    }
+    this.password = generatedPassword.join('');
+
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
 }
