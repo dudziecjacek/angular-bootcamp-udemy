84b2cb040dd26b7dce17a53c7d6df7b33c7aef2e
jdudziec <dudziecjacek@gmail.com>
2020-03-17 11:12:23 +0100

    added ngfor to app html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the beach',
+      // tslint:disable-next-line: max-line-length
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }, {
+      title: 'In the mountains',
+      // tslint:disable-next-line: max-line-length
+      url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }, {
+      title: 'at the city',
+      // tslint:disable-next-line: max-line-length
+      url: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1113&q=80'
+    }, {
+      title: 'In the forest',
+      // tslint:disable-next-line: max-line-length
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    }
+  ];
 
 }
