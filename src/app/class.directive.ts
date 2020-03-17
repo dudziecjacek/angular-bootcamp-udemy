7bed171e1a31f2003eeb4f17cd45d2175b59c164
jdudziec <dudziecjacek@gmail.com>
2020-03-17 12:57:26 +0100

    start

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -8,13 +8,4 @@ export class ClassDirective {
   constructor(private element: ElementRef) {
   }
 
-  @Input('appClass') set classNames(classObj: any) {
-    for (let key in classObj) {
-      if (classObj[key]) {
-        this.element.nativeElement.classList.add(key);
-      } else {
-        this.element.nativeElement.classList.remove(key);
-      }
-    }
-  }
 }
