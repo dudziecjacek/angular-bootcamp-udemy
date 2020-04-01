026150498aa5bcc6742a93c7f2f01b1b36b47896
jdudziec <dudziecjacek@gmail.com>
2020-04-01 13:46:37 +0200

    finished wikisearch with rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from "./wikipedia.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  pages = [];
 
+  constructor(private wikipedia: WikipediaService) { }
+
+
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { PageListComponent } from './page-list/page-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    PageListComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
